a8678dcde017bc4a2c293ef6db241a8049d6bfdb
wukun777 <156493749+wukun777@users.noreply.github.com>
2026-07-07 15:48:39 +0800

    Delete src/PX4-Autopilot directory

--- src/PX4-Autopilot/boards/modalai/voxl2/scripts/build-slpi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "*** Starting qurt slpi build ***"
-
-source /home/build-env.sh
-
-make modalai_voxl2-slpi
-
-echo "*** End of qurt slpi build ***"
--- src/PX4-Autopilot/boards/modalai/voxl2/scripts/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Clean out the build artifacts
-# source /home/build-env.sh
-# make clean
-
-# This gets rid of everything and starts fresh
-sudo rm -fR build
